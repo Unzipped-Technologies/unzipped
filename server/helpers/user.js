3fc0c9c94c7a767d52254d78ea5501ce061a053b
Jason Maynard <jaymaynard84@gmail.com>
2022-12-01 15:43:30 -0500

    feat: init commit

---
+++ server/helpers/user.js
@@ -0,0 +1,44 @@
+
+
+
+// create user
+const createUser = () => {
+    // create User 
+    // create favorites and recently viewed list
+    // create 3rd party application row with googleId if have it
+    // create 3 tags ToDo, In Progress, In Review, Done
+    return;
+}
+
+// update User
+const updateUser = () => {
+    return;
+}
+
+// get User By Id
+const getUserById = () => {
+    return;
+}
+
+// list users
+const listUsers = ({filter, take, skip}) => {
+    console.log({filter, take, skip})
+    return;
+}
+
+// delete users
+const deleteUser = () => {
+    // set inactive, dont actually delete
+    return;
+}
+
+
+
+
+module.exports = {
+    createUser,
+    updateUser,
+    getUserById,
+    listUsers,
+    deleteUser
+}
