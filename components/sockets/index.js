08a1ed60f660f74abbd6f48bc58390490fe0dfd1
HaseebIqbal33 <haseebiqbal33@gmail.com>
2024-04-16 22:55:05 +0500

    Remove Consoles

--- components/sockets/index.js
+++ components/sockets/index.js
@@ -1,9 +1,5 @@
 import socketIOClient from 'socket.io-client'
 import keys from '../../config/keys'
-// Below console is added to check the socket URL for debugging....
-console.log('keys', keys.socketUrl)
-console.log('MONGO_URI', keys?.MONGO_URI, keys?.mongoURI)
-console.log('process.env', process.env)
 const socket = socketIOClient(keys.socketUrl)
 
 export default socket
