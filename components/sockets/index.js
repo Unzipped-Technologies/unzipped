6b208df1180ed6e4034737a66103f481c9c6e2c5
HaseebIqbal33 <haseebiqbal33@gmail.com>
2024-04-16 21:38:37 +0500

    Debugging Sockets URL

--- components/sockets/index.js
+++ components/sockets/index.js
@@ -1,6 +1,7 @@
-import socketIOClient from 'socket.io-client';
+import socketIOClient from 'socket.io-client'
 import keys from '../../config/keys'
+// Below console is added to check the socket URL for debugging....
+console.log('keys', keys.socketUrl)
+const socket = socketIOClient(keys.socketUrl)
 
-const socket = socketIOClient(`${keys.socketUrl}`);
-
-export default socket;
+export default socket
