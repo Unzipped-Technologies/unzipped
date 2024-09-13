3c22f75f67cbb590c1abfadc2e738961f301961e
HaseebIqbal33 <haseebiqbal33@gmail.com>
2024-09-13 16:01:13 +0500

    Add Global Timeout

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,9 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
+    defaultCommandTimeout: 10000,
+    requestTimeout: 10000,
+    responseTimeout: 10000,
     testIsolation: false, // or 'strict' (default)
     setupNodeEvents(on, config) {
       // implement node event listeners here
